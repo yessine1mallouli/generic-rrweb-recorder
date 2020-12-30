a58396e697f69ab1d4b102e5b60d1fd7980fbd21
yessine_mallouli <yessine.mallouli98@gmail.com>
2020-12-30 01:17:48 +0100

    for testing purpose

--- srcs/util.test.js
+++ srcs/util.test.js
@@ -2,4 +2,5 @@ const { logger } = require('./util');
 test('should print something for logging purposes',() => {
     const text = logger("hope its correct");
     expect(text).toBe("generic-rrweb-recorder: hope its correct");
+    
 });
